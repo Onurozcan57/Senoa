9e37ee30893709d477f9038aad336e8b47a74bf2
Onur Özcan <onur.ozcan@istun.edu.tr>
2025-03-23 16:46:14 +0300

    Anasayfa düzenlendi

--- lib/AnaSayfa.dart
+++ lib/AnaSayfa.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, //debug yazısını kaldırdı
+      debugShowCheckedModeBanner: false,
       home: const Anasayfa(),
     );
   }
@@ -25,44 +25,109 @@ class Anasayfa extends StatefulWidget {
 }
 
 class _AnasayfaState extends State<Anasayfa> {
-  // Başlangıçta seçili kart yok
   int _seciliKart = -1;
+  DateTime _selectedDay = DateTime.now();
+  Map<DateTime, List<String>> _tasks = {};
+  TextEditingController _taskController = TextEditingController();
 
-  // Paket kartı oluşturmak için bir widget fonksiyonu
-  Widget PaketKart(String baslik, String aciklama, Color renk, int index) {
-    return GestureDetector(
-      onTap: () {
-        // Kart seçildiğinde setState ile güncellenir
-        setState(() {
-          _seciliKart = index; // Seçili kartın index numarasını güncelle
-        });
-      },
-      child: AnimatedContainer(
-        duration: Duration(milliseconds: 300), // Animasyon süresi
-        width: _seciliKart == index ? 250 : 200, // Seçilen kart genişler
-        height: _seciliKart == index ? 270 : 200, // Seçilen kart büyür
-        margin: EdgeInsets.all(10), // Kartlar arasındaki mesafe
-        decoration: BoxDecoration(
-          color: renk.withOpacity(0.2), // Kart rengi, renk şeffaflık eklenmiş
-          borderRadius: BorderRadius.circular(15), // Kart köşe yuvarlama
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.1), // Gölge rengi
-              blurRadius: 8, // Gölgenin bulanıklık etkisi
-              spreadRadius: 2, // Gölgenin yayılma mesafesi
+  void _showPopup(BuildContext context, String baslik, String aciklama) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        backgroundColor: Colors.transparent, // Arka planı şeffaf yapıyoruz
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        contentPadding: EdgeInsets.zero, // İçeriği tam olarak yerleştiriyoruz
+        content: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15), // Köşeleri yuvarla
+            image: DecorationImage(
+              image: AssetImage('lib/assets/girisekrani.jpg'), // Resmin yolu
+              fit: BoxFit.cover, // Resmi kapsayıcı şekilde yerleştir
             ),
-          ],
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min, // İçeriği minimize et
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  baslik,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white, // Başlık rengi
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text(
+                  aciklama,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.white, // İçerik rengi
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // "Show more" butonuna basıldığında başka bir şey yapılabilir
+                    },
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.blue,
+                      textStyle: TextStyle(fontSize: 16),
+                    ),
+                    child: Text("Show more"),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.red,
+                      textStyle: TextStyle(fontSize: 16),
+                    ),
+                    child: Text("Kapat"),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget PaketKart(
+      String baslik, String aciklama, Color renk, String arkaPlan) {
+    return GestureDetector(
+      onTap: () => _showPopup(context, baslik, aciklama),
+      child: Container(
+        width: 200,
+        height: 200,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15), // Köşeleri yuvarla
+          image: DecorationImage(
+            image: AssetImage(arkaPlan), // Resmin yolu
+            fit: BoxFit.cover, // Resmi kapsayıcı şekilde yerleştir
+          ),
         ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center, // İçeriği ortalamak
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text(
-              baslik, // Başlık kısmı
+              baslik,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-            SizedBox(height: 10), // Başlık ile açıklama arasındaki boşluk
-            Text(aciklama, textAlign: TextAlign.center), // Açıklama kısmı
-            SizedBox(height: 20), // Açıklama ile buton arasındaki boşluk
+            SizedBox(height: 10),
+            Text(aciklama, textAlign: TextAlign.center),
           ],
         ),
       ),
@@ -72,111 +137,152 @@ class _AnasayfaState extends State<Anasayfa> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true, // Klavye açıldığında taşmayı önler
       appBar: AppBar(
-        title: Text('Diyetisyenlik Uygulaması'), // Uygulama başlığı
-        backgroundColor: Color(0xFF34C759), // Canlı yeşil arka plan
+        title: Text('Diyetisyenlik Uygulaması'),
+        backgroundColor: Color(0xFF34C759),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero, // Menünün sıfırlanmış padding değeri
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.green, // Drawer başlık rengi
-              ),
-              child: Text(
-                'Menü Başlığı', // Menü başlığı
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24, // Menü başlığının font boyutu
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(Icons.home), // Ana sayfa ikonu
-              title: Text('Ana Sayfa'), // Menüdeki seçenek adı
-              onTap: () {
-                Navigator.pop(context); // Menü kapatılır
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.settings), // Ayarlar ikonu
-              title: Text('Ayarlar'), // Menüdeki seçenek adı
-              onTap: () {
-                Navigator.pop(context); // Menü kapatılır
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.exit_to_app), // Çıkış ikonu
-              title: Text('Çıkış Yap'), // Menüdeki seçenek adı
-              onTap: () {
-                Navigator.pop(context); // Menü kapatılır
-              },
-            ),
-          ],
-        ),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Color(0xFFF5F5F5), // Hafif gri arka plan
-        ),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            // Takvimi ekliyoruz (Üstte olacak)
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: TableCalendar(
                 firstDay: DateTime.utc(2010, 10, 20),
                 lastDay: DateTime.utc(2060, 10, 20),
-                focusedDay: DateTime.now(),
-                headerVisible: true,
-                daysOfWeekVisible: true, // Günlerin görünürlüğü
-                sixWeekMonthsEnforced: false, // Altı haftalık görünüm
-                shouldFillViewport:
-                    false, // Takvimin ekranın tamamını doldurmaması
+                focusedDay: _selectedDay,
+                selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+                onDaySelected: (selectedDay, focusedDay) {
+                  setState(() {
+                    _selectedDay = selectedDay;
+                  });
+                },
                 headerStyle: HeaderStyle(
                   titleTextStyle: TextStyle(
-                    fontSize: 18, // Başlık boyutunu küçülttük
+                    fontSize: 18,
                     color: Colors.deepPurple,
                     fontWeight: FontWeight.w900,
                   ),
                 ),
                 calendarStyle: CalendarStyle(
                   todayTextStyle: TextStyle(
-                    fontSize:
-                        20, // hangi günde olduğunu belliolsun diye o günün yazısının fontu büyük
+                    fontSize: 20,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
             ),
-            // Başlık Sabit Kalacak
+
+            // Görev ekleme alanı
             Padding(
-              padding: const EdgeInsets.only(top: 0.0),
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _taskController,
+                      decoration: InputDecoration(
+                        labelText: "Görev Ekle",
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.add),
+                    onPressed: () {
+                      if (_taskController.text.isNotEmpty) {
+                        setState(() {
+                          _tasks[_selectedDay] = _tasks[_selectedDay] ?? [];
+                          _tasks[_selectedDay]!.add(_taskController.text);
+                          _taskController.clear();
+                        });
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),
+
+            // Seçilen güne ait görevler
+            SizedBox(
+              height: 200, // Yükseklik vererek overflow hatasını önlüyoruz
+              child:
+                  _tasks[_selectedDay] == null || _tasks[_selectedDay]!.isEmpty
+                      ? Center(child: Text("Bu gün için görev yok."))
+                      : ListView.builder(
+                          itemCount: _tasks[_selectedDay]!.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text(_tasks[_selectedDay]![index]),
+                              trailing: IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red),
+                                onPressed: () {
+                                  setState(() {
+                                    _tasks[_selectedDay]!.removeAt(index);
+                                  });
+                                },
+                              ),
+                            );
+                          },
+                        ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
               child: Text(
                 'Diyet Paketleri',
                 style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
-            // Kartlar için Row
-            Expanded(
+
+            // Paket Kartları
+            SizedBox(
+              height: 250, // Kartların yüksekliği için sabit alan
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
                     PaketKart('Basic Paket', 'Günlük öneriler, basit takip.',
-                        Colors.blue, 0),
+                        Colors.blue, "lib/assets/arkaPlan3.jpg"),
                     PaketKart(
                         'Premium Paket',
                         'Özel diyet listeleri, haftalık analizler.',
                         Colors.green,
-                        1),
+                        "lib/assets/arkaPlan.jpg"),
                     PaketKart(
                         'VIP Paket',
                         'Kişisel diyetisyen, gelişmiş analiz.',
                         Colors.orange,
-                        2),
+                        "lib/assets/arkaPlan2.jpg"),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Text(
+                'Egzersiz Alanı',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 250,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    PaketKart(
+                        'Başlangıç Seviye',
+                        'Hafif egzersizler, temel hareketler.',
+                        Colors.purple,
+                        "lib/assets/girisekrani.jpg"),
+                    PaketKart('Orta Seviye', 'Daha yoğun antrenman programı.',
+                        Colors.cyan, "lib/assets/girisekrani.jpg"),
+                    PaketKart('İleri Seviye', 'Kas geliştirme ve dayanıklılık.',
+                        Colors.amber, "lib/assets/girisekrani.jpg"),
+                    PaketKart('Pro Seviye', 'Sporcular için özel program.',
+                        Colors.teal, "lib/assets/girisekrani.jpg"),
                   ],
                 ),
               ),
