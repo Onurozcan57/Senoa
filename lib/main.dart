ca0ac419f8f3cb6f00664215ef2220bdcdc6e278
Onur Özcan <onur.ozcan@istun.edu.tr>
2025-04-29 23:01:56 +0300

    Akış butonu düzeltme2

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'package:senoa/AnaSayfa.dart';
 import 'package:senoa/DiyAnaSayfa.dart';
 import 'package:senoa/Diyetisyenim.dart';
 import 'package:senoa/LoginScreen.dart';
-import 'package:senoa/Akış.dart';
+import 'package:senoa/AkisSayfasi.dart';
 import 'package:senoa/YemekTarifleri.dart';
 import 'Profile.dart';
 
@@ -21,26 +20,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-import 'package:senoa/AnaSayfa.dart';
-import 'package:senoa/DiyAnaSayfa.dart';
-import 'package:senoa/Diyetisyenim.dart';
-import 'package:senoa/LoginScreen.dart';
-import 'package:senoa/AkisSayfasi.dart';
-import 'Profile.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Anasayfa(),
-    );
-  }
-}
->>>>>>> 36e5268 (Çekmecedeki butonlar halledildi)
